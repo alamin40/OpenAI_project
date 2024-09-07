7554aeec45765f4d0afe210e52b3f5813fc43939
alamin40 <hossain.78295@gmail.com>
2024-09-08 01:07:44 +0200

    adding utils

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from utils import generate_description
+
+# Initialize FastAPI
+app = FastAPI()
+
+# Define your data model for Product
+class Order(BaseModel):
+    product: str
+    units: int
+    
